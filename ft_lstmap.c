50be354c22948060692f51b87b814b8ea6e6fbc2
Arthur MEHMETOGLU <amehmeto@tudent.42.fr>
2017-01-07 02:34:16 +0100

    correction

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: amehmeto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/29 18:09:08 by amehmeto          #+#    #+#             */
-/*   Updated: 2016/12/29 18:27:51 by amehmeto         ###   ########.fr       */
+/*   Updated: 2017/01/06 22:35:33 by amehmeto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	(void)lst;
-	(void)(*f);
-	return (lst);
+	t_list	*tmp;
+
+	tmp = NULL;
+	if (lst && f)
+	{
+		tmp = f(lst);
+		tmp->next = lst->next ? ft_lstmap(lst->next, f) : NULL;
+	}
+	return (tmp);
 }
